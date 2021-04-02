dd452aab26732bb5e9d1e032e645ccb4b32dfcf6
Dhruv Bhanushali <dhruv_b@live.com>
2021-04-02 18:45:04 +0530

    Add aliases for Docker and Docker-Compose

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -13,3 +13,15 @@ alias zshreload="source ~/.zshrc"
 alias pipi="pipenv install"
 alias pipu="pipenv uninstall"
 alias pips="pipenv shell"
+
+# Docker aliases
+alias dock="docker"
+alias dimg="docker image"
+alias dcon="docker container"
+alias dvol="docker volume"
+alias deit="docker exec -ti"
+
+# Docker-Compose aliases
+alias dc="docker-compose"
+alias dce="docker-compose exec"
+alias dcdu="docker-compose down && docker-compose up -d"
