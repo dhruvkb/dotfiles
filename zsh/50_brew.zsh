e90e7b3c29fa8a716ac18284fb562e649ab70d01
Dhruv Bhanushali <dhruv_b@live.com>
2022-01-02 21:48:31 +0400

    Update cURL paths for `brew` on M1

--- zsh/50_brew.zsh
+++ zsh/50_brew.zsh
@@ -1,2 +1,3 @@
-# Use Brew cURL instead of macOS's built-in older version.
-export PATH="/usr/local/opt/curl/bin:$PATH"
+# Use Brew cURL instead of macOS's built-in older version
+# Run `brew install curl` to install it
+export PATH="/opt/homebrew/opt/curl/bin:$PATH"
