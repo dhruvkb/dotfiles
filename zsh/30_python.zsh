966dba3bfd1c4b87358726bcc7918353ed641806
Dhruv Bhanushali <dhruv_b@live.com>
2022-01-02 21:49:11 +0400

    Allow Pipenv to use Python defined in `.python-version`

--- zsh/30_python.zsh
+++ zsh/30_python.zsh
@@ -1,2 +1,5 @@
 # Packages on system-wide Python 3 (eg. Pipenv, HTTPie)
 export PATH="$PATH:/Users/dhruvkb/Library/Python/3.8/bin"
+
+# Configure Pipenv to play nice with Pyenv
+export PIPENV_PYTHON="$(pyenv root)/shims/python"
