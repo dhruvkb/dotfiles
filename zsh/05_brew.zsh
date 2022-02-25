3ad20e441c1827a717130feade38aa4e00f9d595
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-25 16:15:32 +0400

    Move Brew to an increased priority

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -1,3 +1,6 @@
+# Homebrew configuration for M1
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 # Use Brew cURL instead of macOS's built-in older version
 # Run `brew install curl` to install it
 export PATH="/opt/homebrew/opt/curl/bin:$PATH"
