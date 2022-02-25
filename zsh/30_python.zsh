001987f0e14601f66b7b6932d872845a30b95541
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-25 16:16:09 +0400

    Move global packages to pipx

--- zsh/30_python.zsh
+++ zsh/30_python.zsh
@@ -1,5 +1,8 @@
-# Packages on system-wide Python 3 (eg. Pipenv, HTTPie)
-export PATH="$PATH:/Users/dhruvkb/Library/Python/3.8/bin"
+# Pyenv configuration
+eval "$(pyenv init --path)"
+
+# Load pipx binaries on the path, includes utils like HTTPie, Poetry and Pipenv
+export PATH="$PATH:$HOME/.local/bin"
 
 # Configure Pipenv to play nice with Pyenv
 export PIPENV_PYTHON="$(pyenv root)/shims/python"
