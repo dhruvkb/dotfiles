8d6577619bab393bcf52ebfbf06eb91f5110884a
Dhruv Bhanushali <dhruv_b@live.com>
2022-02-25 16:16:29 +0400

    Replace `~` with `$HOME`

--- zsh/99_starship.zsh
+++ zsh/99_starship.zsh
@@ -1,4 +1,4 @@
 # Set the config directory
-export STARSHIP_CONFIG=~/dotfiles/starship/starship.toml
+export STARSHIP_CONFIG="$HOME/dotfiles/starship/starship.toml"
 # Load Starship prompt
 eval "$(starship init zsh)"
