2b276231efbd4bf0277f33e4d82a248a02631b70
Dhruv Bhanushali <dhruv_b@live.com>
2022-07-17 19:28:37 +0400

    Uninstall pyenv

--- zsh/30_python.zsh
+++ zsh/30_python.zsh
@@ -1,8 +1,2 @@
-# Pyenv configuration
-eval "$(pyenv init --path)"
-
-# Load pipx binaries on the path, includes utils like HTTPie, Poetry and Pipenv
+# Load pipx binaries on the path, includes utils like HTTPie, Poetry, Pipenv and PDM
 export PATH="$PATH:$HOME/.local/bin"
-
-# Configure Pipenv to play nice with Pyenv
-export PIPENV_PYTHON="$(pyenv root)/shims/python"
