afe7354434f8c28f7a5d3f8ed8f0571ddcf3ca62
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-16 16:33:56 +0400

    Allow OMZ plugin to configure Brew

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -1,5 +1,9 @@
-# Homebrew configuration for M1
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Homebrew configuration
+# ======================
+
+# The `brew` plugin for Oh My Zsh handles shellenv, so this is not needed.
+# eval "$(/opt/homebrew/bin/brew shellenv)"
+
 
 # Use Brew cURL instead of macOS's built-in older version
 # Run `brew install curl` to install it
