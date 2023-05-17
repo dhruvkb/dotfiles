0a547f6948b11b7173934b30c1b65620c6d7f101
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-17 16:03:09 +0400

    Remove usage of Brew cURL

--- zsh/05_brew.zsh
+++ zsh/05_brew.zsh
@@ -3,8 +3,3 @@
 
 # The `brew` plugin for Oh My Zsh handles shellenv, so this is not needed.
 # eval "$(/opt/homebrew/bin/brew shellenv)"
-
-
-# Use Brew cURL instead of macOS's built-in older version
-# Run `brew install curl` to install it
-export PATH="/opt/homebrew/opt/curl/bin:$PATH"
