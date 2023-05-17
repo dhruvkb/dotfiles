5de6939bd4ffc58a31c95617fe7c2f0f65374bb5
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-18 01:14:38 +0400

    Comment out nvm setup that's handled by OMZ plugin

--- zsh/40_javascript.zsh
+++ zsh/40_javascript.zsh
@@ -1,8 +1,8 @@
-# nvm
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# nvm configuration
+# =================
 
-# Volta
-# export VOLTA_HOME="$HOME/.volta"
-# export PATH="$VOLTA_HOME/bin:$PATH"
+# The `nvm` plugin for Oh My Zsh handles sourcing, loading and completions, so
+# this is not needed.
+# export NVM_DIR="$HOME/.nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
