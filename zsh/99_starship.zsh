9179bb67d4ab61439f10c2bc1d8247f6fa8d4865
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-18 15:43:32 +0400

    Use `{}` for shell interpolation

--- zsh/99_starship.zsh
+++ zsh/99_starship.zsh
@@ -2,7 +2,7 @@
 # ======================
 
 # Set the config directory
-export STARSHIP_CONFIG="$HOME/dotfiles/starship/starship.toml"
+export STARSHIP_CONFIG="${HOME}/dotfiles/starship/starship.toml"
 
 # Load Starship prompt
 eval "$(starship init zsh)"
