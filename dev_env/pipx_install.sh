e4e110486b6defdeb23b9339e5aab4f05740efec
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-20 17:23:05 +0400

    Ensure the presence and ownership of `/opt/pipx`

--- dev_env/pipx_install.sh
+++ dev_env/pipx_install.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+if [ ! -d "/opt/pipx" ]; then
+  sudo mkdir /opt/pipx
+  sudo chown -R "$(whoami)":admin /opt/pipx
+fi
+
 # Install pipx packages
 pipx install poetry \
 && pipx install pipenv \
