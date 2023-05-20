a1723c15e4b616a390b421cf2eb5c86ced88f402
Dhruv Bhanushali <dhruv_b@live.com>
2023-05-20 17:37:48 +0400

    Remove init scripts made redundant by OMZ plugins

--- zsh/05_brew.zsh
+++
@@ -1,5 +0,0 @@
-# Homebrew configuration
-# ======================
-
-# The `brew` plugin for Oh My Zsh handles shellenv, so this is not needed.
-# eval "$(/opt/homebrew/bin/brew shellenv)"
--- zsh/40_javascript.zsh
+++
@@ -1,8 +0,0 @@
-# nvm configuration
-# =================
-
-# The `nvm` plugin for Oh My Zsh handles sourcing, loading and completions, so
-# this is not needed.
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
