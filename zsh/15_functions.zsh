ebc11d2cb7a3407c7767d1ce2499cefb816594a3
Dhruv Bhanushali <dhruv_b@live.com>
2024-04-10 14:54:47 +0400

    Make `goto` capable of running arbitrary commands on directories

--- zsh/15_functions.zsh
+++ zsh/15_functions.zsh
@@ -1,17 +1,20 @@
-# Reads the file `.bookmarks` and provides a short way to jump to the bookmarked
-# directory.
+# Reads the file `.bookmarks` and provides a short way to refer to the
+# bookmarked directory. An optional command can be specified to run instead
+# of `cd` (e.g. to open the directory in an IDE or in Finder).
 #
 # Usage:
-#   goto <name>
+#   goto <name> [<cmd> = cd]
 # where
 #   <name> is the name of the bookmark
+#   <cmd> is the name of the command to run (default: cd)
 function goto() {
   if [[ $# -eq 0 ]]; then
-    echo "Usage: goto <name>"
+    echo "Usage: goto <name> [<cmd> = cd]"
     return
   fi
 
   local name=$1
+  local cmd=${2:-cd}
 
   local bookmarks_file="$HOME/dotfiles/zsh/.bookmarks"
   if [[ ! -f $bookmarks_file ]]; then
@@ -21,7 +24,8 @@ function goto() {
 
   local dir=$(grep "^$name=" "$bookmarks_file" | cut -d'=' -f2-)
   if [[ -n $dir ]]; then
-    cd $dir
+    echo "$cmd \"$dir\"" # Print the command to be executed.
+    $cmd "$dir"
   else
     echo "No directory mapping found for $name."
   fi
