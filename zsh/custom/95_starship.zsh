620fcdfc34b932df3f6ecb86d4b43a2c750533d0
Dhruv Bhanushali <dhruv_b@live.com>
2024-08-07 14:58:25 +0400

    Suppress Starship warnings

--- zsh/custom/95_starship.zsh
+++ zsh/custom/95_starship.zsh
@@ -5,5 +5,8 @@
 # repository.
 export STARSHIP_CONFIG="$HOME/dotfiles/starship/starship.toml"
 
+# Hide warnings like timeouts from the CLI.
+export STARSHIP_LOG="error"
+
 # Load Starship prompt.
 eval "$(starship init zsh)"
