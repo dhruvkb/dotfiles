63b05d8d21e502986b0b3796585a96c54e8a59b2
Dhruv Bhanushali <dhruv_b@live.com>
2024-08-13 06:09:10 +0400

    Move npm config out of the home directory

---
+++ zsh/custom/30_javascript.zsh
@@ -0,0 +1,8 @@
+# JavaScript configuration
+# ========================
+
+# npm
+# ===
+
+# Configure npm to store the config file in the dotfiles repository.
+export NPM_CONFIG_USERCONFIG="$HOME/dotfiles/npm/npmrc"
