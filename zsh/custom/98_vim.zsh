2afc533f17262767d4ca6a1ba655e07bc107d3e0
Dhruv Bhanushali <dhruv_b@live.com>
2024-08-13 06:07:13 +0400

    Move Vim config out of the home directory

---
+++ zsh/custom/98_vim.zsh
@@ -0,0 +1,5 @@
+# Vim configuration
+# =================
+
+# Configure Vim to store the config file in the dotfiles repository.
+export VIMINIT='let $MYVIMRC="$HOME/dotfiles/vim/vimrc" | source $MYVIMRC'
