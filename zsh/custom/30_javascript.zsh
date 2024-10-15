748a604d88fcafde89071f7aeb91aba18a5d2b02
Dhruv Bhanushali <dhruv_b@live.com>
2024-10-15 10:15:55 +0400

    Configure pnpm to use XDG directories

--- zsh/custom/30_javascript.zsh
+++ zsh/custom/30_javascript.zsh
@@ -6,3 +6,12 @@
 
 # Configure npm to store the config file in the dotfiles repository.
 export NPM_CONFIG_USERCONFIG="$HOME/dotfiles/npm/npmrc"
+
+# pnpm
+# ====
+
+# Configure pnpm to use the XDG data directory.
+export PNPM_HOME="/Users/dhruvkb/.local/share/pnpm"
+
+# Add pnpm binaries to the path.
+pathadd $PNPM_HOME
