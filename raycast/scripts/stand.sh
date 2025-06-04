309c06e58d87625deb0ac7e4911cee54cd778852
Dhruv Bhanushali <dhruv_b@live.com>
2025-06-05 01:25:50 +0400

    Refactor scripts and update path to `linak-controller`

--- raycast/scripts/stand.sh
+++ raycast/scripts/stand.sh
@@ -9,12 +9,21 @@
 # @raycast.icon ⬆️
 # @raycast.packageName Idåsen
 
+# Environment variables are not populated in Raycast scripts.
 XDG_STATE_HOME="$HOME/.local/state"
-mkdir -p $XDG_STATE_HOME/raycast
-/opt/pipx/bin/linak-controller \
+
+# Ensure that the log directory exists.
+mkdir -p "$XDG_STATE_HOME/raycast"
+
+# Write timestamp to log file.
+echo "$(date) stand.sh" >> "$XDG_STATE_HOME/raycast/stand.log"
+
+# Run the linak-controller command in background with output redirected to log file.
+/opt/uv/bin/linak-controller \
   --mac-address 84F900A3-AB7F-C330-501D-575AB6D66797 \
   --move-to 1020 \
-	&>"$XDG_STATE_HOME/raycast/stand.log" \
+  >>"$XDG_STATE_HOME/raycast/stand.log" \
+  2>&1 \
   &
 
 # This line will be shown in a toast message.
