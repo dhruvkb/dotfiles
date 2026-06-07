773c745e4b777de1b0e8eba71675deae64261f54
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-07 14:19:22 +0400

    Enable completions without Oh My Zsh

---
+++ zsh/custom/compinit.zsh
@@ -0,0 +1,29 @@
+# Compinit
+# ========
+
+autoload -Uz compinit
+
+_zcompdump="$XDG_CACHE_HOME/zsh/zcompdump-$ZSH_VERSION"
+
+# Ensure that the directory part of `_zcompdump` exists.
+mkdir -p "${_zcompdump:h}"
+
+if [[ -n "$_zcompdump"(#qN.mh+24) ]]; then
+  # The file is stale, so we need to do a full security check and rebuild the cache.
+  compinit -d "$_zcompdump"
+else
+  # The file is fresh, so we can trust it and skip the slow security check.
+  compinit -C -d "$_zcompdump"
+fi
+
+# Use Shift ⇧ + Tab ⇥ to go backwards in completion lists
+zmodload -i zsh/complist # Only needed for `bindkey`.
+bindkey -M menuselect '^[[Z' reverse-menu-complete
+
+# Show completion as a menu with selectable options.
+zstyle ':completion:*:*:*:*:*' menu select
+
+# This is case-insensitive and hyphen/underscore-insensitive completion.
+zstyle ':completion:*' matcher-list 'm:{[:lower:][:upper:]-_}={[:upper:][:lower:]_-}' 'r:|=*' 'l:|=* r:|=*'
+
+unset _zcompdump
