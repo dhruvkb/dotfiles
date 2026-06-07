52ea28831cbcd4867e89bed6aeb18f1066de2493
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-07 14:13:42 +0400

    Bring 1Password CLI's aliased versions into dotfiles

--- zsh/custom/91_1pass.zsh
+++
@@ -1,9 +0,0 @@
-# 1Password configuration
-# =======================
-
-# Include 1Password plugins configuration.
-if [ -e "$XDG_CONFIG_HOME/op/plugins.sh" ]; then
-	source "$XDG_CONFIG_HOME/op/plugins.sh"
-else
-	echo "1Password shell plugins file not found."
-fi
