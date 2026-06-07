2269025fc2ddcc7faf33415efbd991172c944b5e
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-07 14:18:19 +0400

    Keep aliases at the end of the file

--- zsh/custom/1pass.zsh
+++ zsh/custom/1pass.zsh
@@ -1,8 +1,8 @@
 # 1Password configuration
 # =======================
 
-# Alias CLI tools to authenticate using 1Password.
-alias gh="op plugin run -- gh"
-
 # Tell 1Password CLI that the aliases have already been set up.
 export OP_PLUGINS_SOURCED="1"
+
+# Aliases
+alias gh="op plugin run -- gh"
