0314f69b6b920981e5ffaaa66ee61a473e0fc6f4
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-07 14:12:29 +0400

    Configure Git to read from dotfiles and reduce a symlink

---
+++ zsh/custom/git.zsh
@@ -0,0 +1,21 @@
+# Git
+# ===
+
+# Configure Git to store the global config file in the dotfiles repository.
+export GIT_CONFIG_GLOBAL="$HOME/dotfiles/git/config"
+
+# Expose custom `git-<name>` subcommands.
+pathadd "$HOME/dotfiles/git/bin"
+
+# Aliases
+alias g="git"
+alias ga="git add"
+alias gc="git commit"
+alias gcmsg="git commit -m"
+alias glo="git log --oneline --graph --decorate"
+alias gco="git checkout"
+alias gcb="git checkout -b"
+alias gp="git push"
+alias gpf="git push --force-with-lease"
+alias gpsup='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
+alias gpl="git pull"
