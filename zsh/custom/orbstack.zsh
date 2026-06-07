2cecd06ab1886d48851f8f26840764cd06fd65b0
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-07 14:42:09 +0400

    Use Bake with Compose

--- zsh/custom/orbstack.zsh
+++ zsh/custom/orbstack.zsh
@@ -7,6 +7,10 @@
 # Configure Docker to use XDG config directory.
 export DOCKER_CONFIG="$XDG_CONFIG_HOME/docker"
 
+# Use Bake to orchestrate build execution in the most efficient way.
+# https://docs.docker.com/compose/how-tos/dependent-images/#build-with-bake
+export COMPOSE_BAKE="true"
+
 # Update path to include CLI tools like `docker`, `kubectl`, `orb` etc.
 if [[ ":$PATH:" != *":$HOME/.orbstack/bin:"* ]]; then
 	if [ -e "$HOME/.orbstack/shell/init.zsh" ]; then
