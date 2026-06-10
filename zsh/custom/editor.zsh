f0e181706824e523754931e16dd0eb73ba95d9c2
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-10 13:59:16 +0400

    Use `$+commands` to not match aliases of the same names

--- zsh/custom/editor.zsh
+++ zsh/custom/editor.zsh
@@ -1,10 +1,14 @@
 # Editor
 # ======
 
-# Set preferred editor for local and remote sessions. We prefer `hx` over `vim` over `vi`.
-if $(command -v hx &>/dev/null); then
+# Set preferred editor for local and remote sessions. The order of preference is
+# `hx` > `vim` > `vi`.
+#
+# We use `$+commands` because it checks for real executables only and does not
+# match the aliases defined below.
+if (( $+commands[hx] )); then
   export EDITOR="hx"
-elif $(command -v vim &>/dev/null); then
+elif (( $+commands[vim] )); then
   export EDITOR="vim"
 else
   export EDITOR="vi"
