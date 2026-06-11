d77a1eab0221582e4a4e0bdbfdb0622765aae345
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-12 03:14:56 +0400

    Avoid hardcoding path to uv tools

--- bootstrap/raycast.zsh
+++ bootstrap/raycast.zsh
@@ -7,7 +7,7 @@ source "${0:A:h}/_common.zsh"
 printf "Populating desk UUID..."
 # Populate the desk UUID
 mkdir -p ~/dotfiles/raycast/data/
-desk_uuid=$(/opt/uv/bin/linak-controller --scan \
+desk_uuid=$($UV_TOOL_BIN_DIR/linak-controller --scan \
 	| grep "Desk " \
 	| awk -F':' '{print $1}')
 echo "DESK_UUID='$desk_uuid'" > ~/dotfiles/raycast/data/desk.sh
