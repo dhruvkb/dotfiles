8b1464efeef5f7d6174ea71bf6ea57d82f922c91
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-12 03:48:53 +0400

    Add a script to clean junk files left in `$HOME`

---
+++ bootstrap/cleanup.zsh
@@ -0,0 +1,10 @@
+#!/usr/bin/env zsh
+
+# This script cleans up junk files from the home directory.
+
+source "${0:A:h}/_common.zsh"
+
+printf 'Clearing up junk files...'
+# Remove the junk files created before the symlink existed.
+rm -rf "$HOME/.zsh_history" "$HOME/.zsh_sessions" "$HOME/.lesshst"
+green 'done.\n'
