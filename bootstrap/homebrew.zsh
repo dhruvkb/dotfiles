8753788bcb632e6501fb8f17ed9ede4850f60dc3
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-12 04:03:52 +0400

    Install formulae and casks with Homebrew

---
+++ bootstrap/homebrew.zsh
@@ -0,0 +1,85 @@
+#!/usr/bin/env zsh
+
+# This script installs or, if already present, updates Homebrew.
+# Then it installs, and if necessary, updates the listed formulae and casks.
+
+source "${0:A:h}/_common.zsh"
+
+# Since we cannot rely on `brew` to be on the path, we directly check if the
+# binary exists at `/opt/homebrew/bin/brew`.
+if [ -x /opt/homebrew/bin/brew ]; then
+	green "Homebrew is already installed.\n"
+
+	printf '┌─ Updating Homebrew...\n'
+	indent /opt/homebrew/bin/brew update
+	green '└─ done.\n'
+else
+	printf '┌─ Installing Homebrew...\n'
+	# This does not follow `curl | bash` because it needs `sudo` access.
+	bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	green '└─ done.\n'
+fi
+
+printf '┌─ Installing Homebrew formulae...\n'
+# Install Brew formulae
+FORMULAE=(
+	anomalyco/tap/opencode
+	atuin
+	awscli
+	bat
+	coreutils
+	eza
+	ffmpeg
+	fnm
+	gh
+	git-crypt
+	git-delta
+	gnupg
+	hashicorp/tap/terraform
+	helix
+	jq
+	just
+	pls-rs/pls/pls
+	rustup
+	starship
+	uv
+	xh
+	yt-dlp
+	zx
+)
+indent /opt/homebrew/bin/brew install ${FORMULAE[@]}
+green '└─ done.\n'
+
+printf '┌─ Installing Homebrew casks...\n'
+# Install Brew casks
+CASKS=(
+	1password
+	1password-cli
+	arc
+	beeper
+	bettermouse
+	claude-code
+	cloudflare-warp
+	coteditor
+	font-inter
+	font-monaspace
+	font-symbols-only-nerd-font
+	ghostty
+	hammerspoon
+	keyboardcleantool
+	latest
+	obsidian
+	openmtp
+	orbstack
+	orion
+	slack
+	transmission
+	tunnelblick
+	visual-studio-code
+	vlc
+	whatsapp
+	yaak
+	zed
+)
+indent /opt/homebrew/bin/brew install --cask ${CASKS[@]}
+green '└─ done.\n'
