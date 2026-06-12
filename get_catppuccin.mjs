5a37c5bdfa7a0f1b27454e99e707fe7c22e1c697
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-12 05:24:14 +0400

    Delete unused, incomplete script

--- get_catppuccin.mjs
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zx
-
-const downloads = [];
-
-for (const download of downloads) {
-	const { url, dest } = download;
-	await $`mkdir -p ${path.dirname(dest)}`;
-	await $`curl -fsSL -o ${dest} ${url}`;
-}
