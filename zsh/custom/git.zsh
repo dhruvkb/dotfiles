4b8984fbe65371a655faf04d2bdd9cc7cd7a2306
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-13 01:13:04 +0400

    Make `gh` read its config directly from this repo

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -19,3 +19,9 @@ alias gp="git push"
 alias gpf="git push --force-with-lease"
 alias gpsup='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
 alias gpl="git pull"
+
+# GitHub CLI
+# ==========
+
+# Configure GitHub CLI to store its configs file in the dotfiles repository.
+export GH_CONFIG_DIR="$HOME/dotfiles/gh"
