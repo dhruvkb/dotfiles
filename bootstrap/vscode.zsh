6bd18dc48a541fa43edcc18d260b72f817baca24
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-12 04:04:40 +0400

    Automatically install the important VS Code extensions

---
+++ bootstrap/vscode.zsh
@@ -0,0 +1,35 @@
+#!/usr/bin/env zsh
+
+# This script sets up Visual Studio Code.
+
+source "${0:A:h}/_common.zsh"
+
+EXTENSIONS=(
+	# Themes
+	Catppuccin.catppuccin-vsc
+	Catppuccin.catppuccin-vsc-icons
+	# Editor
+	EditorConfig.EditorConfig
+	# Languages
+	tamasfe.even-better-toml
+	ms-python.python
+	rust-lang.rust-analyzer
+	unifiedjs.vscode-mdx
+	# Frameworks
+	Vue.volar
+	astro-build.astro-vscode
+	# Linters & formatters
+	dbaeumer.vscode-eslint
+	esbenp.prettier-vscode
+	oxc.oxc-vscode
+	# Tools
+	ms-azuretools.vscode-containers
+)
+
+printf '┌─ Installing extensions...\n'
+for extension in ${EXTENSIONS[@]}; do
+	indent code --install-extension $extension --force
+done
+green '└─ done.\n'
+
+yellow 'DISABLE ALL NON-UNIVERSAL EXTENSIONS!\n'
