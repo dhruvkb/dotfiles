41da44c80b85de2442506f176cc04bfb5c405b6c
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-14 11:08:48 +0400

    Fix a bug in the use of `${+commands[...]}`

--- zsh/custom/editor.zsh
+++ zsh/custom/editor.zsh
@@ -4,11 +4,11 @@
 # Set preferred editor for local and remote sessions. The order of preference is
 # `hx` > `vim` > `vi`.
 #
-# We use `$+commands` because it checks for real executables only and does not
-# match the aliases defined below.
-if (($+commands[hx])); then
+# We use `${+commands[...]}` because it checks for real executables only and
+# does not match the aliases defined below.
+if ((${+commands[hx]})); then
 	export EDITOR="hx"
-elif (($+commands[vim])); then
+elif ((${+commands[vim]})); then
 	export EDITOR="vim"
 else
 	export EDITOR="vi"
