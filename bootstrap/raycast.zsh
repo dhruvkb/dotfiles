29d807e1220ddaea83933cbcbd2f4ed78e61b43a
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-14 04:52:55 +0400

    Combine `grep` and `awk` to simplify desk UUID extraction

--- bootstrap/raycast.zsh
+++ bootstrap/raycast.zsh
@@ -7,8 +7,6 @@ source "${0:A:h}/_common.zsh"
 print -rn -- "Populating desk UUID..."
 # Populate the desk UUID
 mkdir -p ~/dotfiles/raycast/data/
-desk_uuid=$($UV_TOOL_BIN_DIR/linak-controller --scan |
-	grep "Desk " |
-	awk -F':' '{print $1}')
+desk_uuid=$($UV_TOOL_BIN_DIR/linak-controller --scan | awk -F':' '/Desk / {print $1}')
 print -r -- "DESK_UUID='$desk_uuid'" >~/dotfiles/raycast/data/desk.sh
 green "done.\n"
