1d73b642e22878825ac5d0727ade07cb141cf085
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-16 02:25:57 +0400

    Fix typo in 1Password's `plugins.sh` script

--- zsh/custom/op.zsh
+++ zsh/custom/op.zsh
@@ -1,9 +1,9 @@
 # 1Password configuration
 # =======================
 
-# Source 1Password CLI's `plugin.sh` if it exists.
-if [[ -f "$XDG_CONFIG_HOME/op/plugin.sh" ]]; then
-	source "$XDG_CONFIG_HOME/op/plugin.sh"
+# Source 1Password CLI's `plugins.sh` if it exists.
+if [[ -f "$XDG_CONFIG_HOME/op/plugins.sh" ]]; then
+	source "$XDG_CONFIG_HOME/op/plugins.sh"
 
 	# Mark the state to know that the sourcing was successful.
 	export OP_PLUGIN_SOURCED=1
