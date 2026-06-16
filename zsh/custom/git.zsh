67dfc1350e89f820551c0d7ce427387687421538
Dhruv Bhanushali <dhruv_b@live.com>
2026-06-17 03:30:15 +0400

    Update Git aliases

--- zsh/custom/git.zsh
+++ zsh/custom/git.zsh
@@ -7,18 +7,37 @@ export GIT_CONFIG_GLOBAL="$HOME/dotfiles/git/config"
 # Expose custom `git-<name>` subcommands.
 pathadd "$HOME/dotfiles/git/bin"
 
+# Get the name of the current Git branch.
+#
+# Usage
+#   git_current_branch
+git_current_branch() {
+	local ref
+	ref=$(__git_prompt_git symbolic-ref --quiet HEAD 2>/dev/null)
+	local ret=$?
+	if [[ $ret != 0 ]]; then
+		[[ $ret == 128 ]] && return # no git repo.
+		ref=$(__git_prompt_git rev-parse --short HEAD 2>/dev/null) || return
+	fi
+	echo ${ref#refs/heads/}
+}
+
 # Aliases
 alias g="git"
 alias ga="git add"
 alias gc="git commit"
-alias gcmsg="git commit -m"
+alias gcm="git commit --message"
+alias gcf="git commit --fixup"
+alias gca="git commit --amend"
+alias gl="git log --graph --decorate"
 alias glo="git log --oneline --graph --decorate"
 alias gco="git checkout"
-alias gcb="git checkout -b"
+alias gcob="git checkout -b"
 alias gp="git push"
 alias gpf="git push --force-with-lease"
-alias gpsup='git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)'
+alias gps='git push --set-upstream origin $(git_current_branch)'
 alias gpl="git pull"
+alias gplr="git pull --rebase"
 
 # GitHub CLI
 # ==========
